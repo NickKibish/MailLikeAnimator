4d8ba49d5bdacec42b3047300c7b9e88a68f36cf
Nick Kibish <nickkibish@MacBook-Pro-Nick.local>
2016-09-02 18:50:57 +0300

    ment

---
+++ Example/Pods/Target Support Files/MailLikeAnimator/MailLikeAnimator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MailLikeAnimator : NSObject
+@end
+@implementation PodsDummy_MailLikeAnimator
+@end
---
+++ Example/Pods/Target Support Files/MailLikeAnimator/MailLikeAnimator-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double MailLikeAnimatorVersionNumber;
+FOUNDATION_EXPORT const unsigned char MailLikeAnimatorVersionString[];
+
