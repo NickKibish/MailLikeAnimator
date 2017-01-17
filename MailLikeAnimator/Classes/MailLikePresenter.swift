0c8b7d388e0abb889c529085e4d85d707a98ed5e
Nick Kibish <nickkibish@MacBook-Pro-Nick.local>
2017-01-17 12:51:32 +0200

    updated library to swift 3

--- MailLikeAnimator/Classes/MailLikePresenter.swift
+++ MailLikeAnimator/Classes/MailLikePresenter.swift
@@ -10,61 +10,61 @@ import UIKit
 private let viewTag = 101
 
 public class MailLikePresenter: NSObject {
-    var animationDuration: NSTimeInterval = 0.5
+    var animationDuration: TimeInterval = 0.5
     var topMargin: CGFloat = 50
     var presentingVCAlpha: CGFloat = 0.7
     var scale: CGFloat = 0.9
 }
 
 extension MailLikePresenter: UIViewControllerAnimatedTransitioning {
-    public func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+    public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return animationDuration
     }
     
-    public func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-        guard let fromVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey),
-        let containerView = transitionContext.containerView(),
-        let toVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) else {
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from),
+        let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to) else {
             return
         }
         
-        var initialFrame = UIScreen.mainScreen().bounds
+        let containerView = transitionContext.containerView
+        var initialFrame = UIScreen.main.bounds
         initialFrame.size.height -= topMargin
         initialFrame.origin.y = initialFrame.height
         
         var finalFrame = initialFrame
         finalFrame.origin.y = topMargin
         
-        let snapshot = toVC.view.snapshotViewAfterScreenUpdates(true)
-        snapshot.frame = initialFrame
+        let snapshot = toVC.view.snapshotView(afterScreenUpdates: true)
+        snapshot?.frame = initialFrame
         
-        let fromSnapshot = fromVC.view.snapshotViewAfterScreenUpdates(true)
+        let fromSnapshot = fromVC.view.snapshotView(afterScreenUpdates: true)
         
         containerView.addSubview(toVC.view)
-        containerView.addSubview(snapshot)
-        toVC.view.hidden = true
+        containerView.addSubview(snapshot!)
+        toVC.view.isHidden = true
         
-        let duration = transitionDuration(transitionContext)
+        let duration = transitionDuration(using: transitionContext)
         
-        fromSnapshot.transform = CGAffineTransformMakeScale(scale, scale)
-        fromSnapshot.alpha = presentingVCAlpha
+        fromSnapshot?.transform = CGAffineTransform(scaleX: scale, y: scale)
+        fromSnapshot?.alpha = presentingVCAlpha
         
-        UIView.animateWithDuration(duration, animations: { [weak self] in
+        UIView.animate(withDuration: duration, animations: { [weak self] in
             guard let alpha = self?.presentingVCAlpha,
             let scale = self?.scale else {
                 return
             }
             
-            snapshot.frame = finalFrame
-            fromVC.view.transform = CGAffineTransformMakeScale(scale, scale)
+            snapshot?.frame = finalFrame
+            fromVC.view.transform = CGAffineTransform(scaleX: scale, y: scale)
             fromVC.view.alpha = alpha
             
         }) { (completed) in
-            fromSnapshot.tag = viewTag
-            containerView.insertSubview(fromSnapshot, atIndex: 0)
-            snapshot.removeFromSuperview()
+            fromSnapshot?.tag = viewTag
+            containerView.insertSubview(fromSnapshot!, at: 0)
+            snapshot?.removeFromSuperview()
             toVC.view.frame = finalFrame
-            toVC.view.hidden = false
+            toVC.view.isHidden = false
             
             transitionContext.completeTransition(completed)
         }
